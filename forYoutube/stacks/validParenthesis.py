737dd605d40fff23cb0395427f15a83bdceb7969
clecobby <pharaoh@Pharaohs-MacBook-Pro.local>
2024-10-27 13:26:05 +0000

    new ones

--- forYoutube/stacks/validParenthesis.py
+++ forYoutube/stacks/validParenthesis.py
@@ -5,9 +5,16 @@ class Solution:
         for c in s:
             if c not in Map:
                 stack.append(c)
+                print (f"****{c}")
                 continue
             if not stack or stack[-1] != Map[c]:
+                print (f"****%%%{c}")
                 return False
             stack.pop()
-
+        print (f"2323")
         return not stack
+    
+if __name__=="__main__":
+    solution = Solution()
+    results=solution.isValid('[)(]')
+    print(results)
